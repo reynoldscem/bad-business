9a19539b0eaa31b8df90c470d58e4f4887f9b91b
Charles EM Reynolds <reynoldscem@gmail.com>
2018-01-30 13:34:47 +0000

    Initial

---
+++ bad_business.py
@@ -0,0 +1,161 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import numpy as np
+import re
+
+ADVENTURING_GEAR_URL = 'https://open5e.com/equipment/adventuring-gear.html'
+TRADE_GOODS_URL = 'https://dnd5e.info/equipment/trade-goods'
+
+
+class Item():
+    @staticmethod
+    def parse_weight(weight_string):
+        if 'lb' not in weight_string:
+            return 0.
+
+        # Edge case of accidentally leading text before weight.
+        weight_string = re.findall('\D*(\d+.*)', weight_string)[0]
+
+        weight_string, *_ = weight_string.split()
+        weight_string = weight_string.replace('½', '.5')
+
+        fraction_match = re.match('(:?\d+)/(:?\d+)', weight_string)
+        if fraction_match is None:
+            weight = float(weight_string)
+        else:
+            numerator, denominator = map(int, fraction_match.groups())
+            weight = numerator / denominator
+
+        return weight
+
+    @staticmethod
+    def parse_cost(cost_string):
+        cost_string = cost_string.replace(',', '')
+        expression = re.compile('(:?\d+) (:?[csegp])p?.*')
+        multipliers = {
+            'c': 1,
+            's': 10,
+            'e': 50,
+            'g': 100,
+            'p': 1000
+        }
+        match = re.match(expression, cost_string)
+
+        if match is None:
+            raise Exception
+
+        value_string, units_string = match.groups()
+        value = int(value_string)
+
+        copper_value = value * multipliers[units_string]
+
+        return copper_value
+
+    def __init__(self, name, cost, weight, scaling_factor=0.5):
+        self.name = name
+        self.cost = self.parse_cost(cost)
+        self.weight = self.parse_weight(weight)
+
+        self.cost_ratio = (self.cost) / (self.weight + scaling_factor)
+
+    def __str__(self):
+        return (
+            'Name:\t{}\n\nCost:\t{}cp\n\n'
+            'Weight:\t{}lbs.\n\nRatio:\t{:.4f}\n'
+            ''.format(self.name, self.cost, self.weight, self.cost_ratio)
+        )
+
+
+def main():
+    response = urlopen(ADVENTURING_GEAR_URL)
+    data = response.read()
+    soup = BeautifulSoup(data, 'html.parser')
+
+    table_div = soup.find('div', {'id': 'adventuring-gear'})
+    table_rows = table_div.table.tbody.find_all('tr')
+    table_entries = [
+        table_row.find_all('td')
+        for table_row in table_rows
+    ]
+
+    def tag_text(tag):
+        if tag.text == '\xa0':
+            raise Exception
+        else:
+            return tag.text
+
+    def valid_entry(entry):
+        num_fields = 3
+        space_character = '\xa0'
+
+        if len(entry) != num_fields:
+            return False
+
+        if entry[1].text == space_character:
+            return False
+
+        if entry[2].text == space_character:
+            return False
+
+        return True
+
+    table_entries = [
+        list(map(tag_text, entry))
+        for entry in table_entries
+        if valid_entry(entry)
+    ]
+
+    items = [
+        Item(name, cost, weight)
+        for (name, cost, weight) in table_entries
+    ]
+
+    ratios = [item.cost_ratio for item in items]
+    ratio_total = sum(ratios)
+    normalised_ratios = np.array(ratios) / ratio_total
+
+    cost_budget = 250 * 100
+    weight_budget = 420
+
+    current_cost = 0
+    current_weight = 0
+
+    max_violations = 100
+
+    probability_to_choose_uniformly = 0.9
+
+    violations = 0
+    selected_items = []
+    while violations < max_violations:
+        if np.random.uniform() < probability_to_choose_uniformly:
+            next_item = np.random.choice(items)
+        else:
+            next_item = np.random.choice(items, p=normalised_ratios)
+
+        if current_cost + next_item.cost > cost_budget:
+            violations += 1
+            continue
+        if current_weight + next_item.weight > weight_budget:
+            violations += 1
+            continue
+
+        violations = 0
+        current_cost += next_item.cost
+        current_weight += next_item.weight
+        selected_items.append(next_item)
+
+    from collections import Counter
+    for item, count in Counter(selected_items).items():
+        if count > 1:
+            print('{} units of:'.format(count))
+        print(item)
+
+    print('Total weight:\t{:.2f} lbs.'.format(current_weight))
+    print('Total cost:\t{:.2f} gp'.format(current_cost / 100))
+
+    import IPython
+    IPython.embed()
+
+
+if __name__ == '__main__':
+    main()
